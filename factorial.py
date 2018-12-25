8ca1a5d02c95c211b403127b50a70cdfde86f2cd
CuiiGen <46126598+CuiiGen@users.noreply.github.com>
2018-12-25 19:04:16 +0800

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def fact(n):
+    result = 1
+
+    for i in range(1, n + 1):
+        result *= i
+
+    print(result)
+
+
+def main():
+    fact(8)
+
+
+if __name__ == '__main__':
+    main()
