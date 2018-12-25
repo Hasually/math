694a9c4aa12b8474ce423b85f4c6320b3c73fc3e
Hasually <46130700+Hasually@users.noreply.github.com>
2018-12-25 19:17:32 +0800

    Update factorial.py
    Some comment

--- factorial.py
+++ factorial.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 def fact(n):
+    
+    # 保存最终结果
     result = 1
-
+    
+    # 进行循环控制
     for i in range(1, n + 1):
         result *= i
-
+    
+    # 打印输出
     print(result)
 
 
